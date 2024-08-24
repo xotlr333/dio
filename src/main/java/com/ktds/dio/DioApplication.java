3179b0fb76d9e86ae5c9887f783682e0207d60a9
TAESIC KONG <81614820+xotlr333@users.noreply.github.com>
2024-08-24 14:00:17 +0900

    Add files via upload

---
+++ src/main/java/com/ktds/dio/DioApplication.java
@@ -0,0 +1,13 @@
+package com.ktds.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ktds/dio/DioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktds.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
